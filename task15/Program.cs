8fa90182e75cb0c8d7ecef7b22df560ba2d3b0e6
pavel <pavel-babin@yandex.ru>
2022-07-13 18:54:53 +0500

    Task №15

---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программa, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+Console.WriteLine("Введите номе от 1 до 7");
+int nomber = int.Parse(Console.ReadLine());
+
+if (nomber > 7 ) 
+{
+    Console.WriteLine("ЭТА ЦЫФРА НЕ ПРИНАДЛЕЖИТ НИ ОДНОМУ ДНЮ НЕДЕЛИ");
+}    
+else
+{
+    if (nomber == 1) Console.WriteLine("Будни");
+    if (nomber == 2) Console.WriteLine("Будни");
+    if (nomber == 3) Console.WriteLine("Будни");
+    if (nomber == 4) Console.WriteLine("Будни");
+    if (nomber == 5) Console.WriteLine("Будни");
+    if (nomber == 6) Console.WriteLine("Выходной");
+    if (nomber == 7) Console.WriteLine("Выходной");
+}
