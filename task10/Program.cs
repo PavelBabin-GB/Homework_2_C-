609b28fb281c262a3e1d2a22ba87a57ed4b378e9
pavel <pavel-babin@yandex.ru>
2022-07-13 16:26:29 +0500

    Task №10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine((number/10)%10);
+}    
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+} 
