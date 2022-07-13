0026bca2763791c58158d935ace12ea5586debcb
pavel <pavel-babin@yandex.ru>
2022-07-13 18:26:23 +0500

    Task №13

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 99)
+{
+while (number > 999)
+   {  
+       number = number/10;  
+   }
+  Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("В этом числе нет третьей цыфры");
+} 
+ 
