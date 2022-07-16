039900fd6aa3d8629863f9829c63dec6314ffee9
pavel <pavel-babin@yandex.ru>
2022-07-16 16:11:31 +0500

    Исправил формат

--- task13/Program.cs
+++ task13/Program.cs
@@ -4,10 +4,10 @@ int number = int.Parse(Console.ReadLine());
 if (number > 99)
 {
 while (number > 999)
-   {  
-       number = number/10;  
-   }
-  Console.WriteLine(number%10);
+{  
+    number /= 10;  
+}
+Console.WriteLine(number % 10);
 }
 else
 {
